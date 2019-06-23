1921c810572044f5d8e5e05d7be289c688d2a1b3
YuSan <ysoga19@gmail.com>
2019-06-24 00:51:43 +0900

    Rewrite index.html and other one  for new design

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,15 @@
-$(document).ready(function () {
+$(document).ready(() => {
   $(".responsive-iframe").cbIframeSize();
 });
+
+$('.navbar-burger').on('click', e => {
+  $('#navBurger').hasClass('is-active') ? $('#navBurger').removeClass('is-active') : $('#navBurger').addClass('is-active')
+})
+
+$('.hero-foot>.tabs .tab').on('click', function() {
+  $('.hero-foot>.tabs .tab').removeClass('is-active')
+  $(this).addClass('is-active')
+  $('main>section').addClass('is-hidden')
+  const displayElement = $(this).attr('data-target')
+  $(`#${displayElement}`).removeClass('is-hidden')
+})
