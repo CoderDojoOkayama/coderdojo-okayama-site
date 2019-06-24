33d72fd925687c2d199697f99f6e4dcdcbc1f527
YuSan <ysoga19@gmail.com>
2019-06-25 00:09:18 +0900

    Update index,html and add list of Dojos

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
   $(".responsive-iframe").cbIframeSize();
+  let hash = $(window.location.hash).is('*') ? window.location.hash : '#overview'
+  window.location.hash = hash
+  setDefaultHeroTab(hash)
 });
 
 $('.navbar-burger').on('click', e => {
@@ -11,5 +14,16 @@ $('.hero-foot>.tabs .tab').on('click', function() {
   $(this).addClass('is-active')
   $('main>section').addClass('is-hidden')
   const displayElement = $(this).attr('data-target')
+  window.location.hash = displayElement
   $(`#${displayElement}`).removeClass('is-hidden')
 })
+
+function setDefaultHeroTab(hash) {
+  id = hash.split('#').join('')
+
+  $('.hero-foot>.tabs .tab').removeClass('is-active')
+  $(`.hero-foot>.tabs .tab-${ id }`).addClass('is-active')
+
+  $('main>section').addClass('is-hidden')
+  $(hash).removeClass('is-hidden')
+}
