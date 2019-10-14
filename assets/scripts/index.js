baa8a7c30e78a069f5c226cdd46353339ba4ccc7
YuSan <ysoga19@gmail.com>
2019-10-14 21:53:44 +0900

    トップページの navbar がスクロールすると色変わるようにした

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,10 @@
+// tabs
+$(document).ready(() => {
+  let hash = $(window.location.hash).is('*') ? window.location.hash : '#overview'
+  window.location.hash = hash
+  setDefaultHeroTab(hash)
+});
+
 $('.hero-foot>.tabs .tab').on('click', function() {
   $('.hero-foot>.tabs .tab').removeClass('is-active')
   $(this).addClass('is-active')
@@ -16,3 +23,17 @@ function setDefaultHeroTab(hash) {
   $('main>section').addClass('is-hidden')
   $(hash).removeClass('is-hidden')
 }
+
+// navbar scroll
+$(document).ready(function(){
+  let main_offset = $('main').offset()
+
+  $(document).scroll(function(){
+    scroll_start = $(document).scrollTop();
+    if (scroll_start > main_offset.top) {
+      $('.navbar').addClass('is-colored')
+    } else {
+      $('.navbar').removeClass('is-colored')
+    }
+  })
+})
